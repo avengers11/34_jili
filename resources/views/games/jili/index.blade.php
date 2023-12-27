29e31ca59ff575b66196d83aeb5489b8ff6f7a95
masudranadeveloper <masud1010rana0101@gmail.com>
2023-12-27 16:39:06 +0600

    okay

--- resources/views/games/jili/index.blade.php
+++ resources/views/games/jili/index.blade.php
@@ -16,7 +16,7 @@
     @if (env('MR_ENV') == "local")
         @vite(['resources\js\JiliGames.js'])
     @else
-        <script src="{{ asset('build/assets/app-3ee30b31.js') }}"></script>
+        <script src="{{ asset('build\assets\app-87057329.js') }}"></script>
     @endif
 
 </head>
